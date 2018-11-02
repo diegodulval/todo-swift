0b1483367480c58036ff90acf4689a6252f63fa0
Dulval <dulval@Mac-mini-de-Rafael.local>
2018-11-02 12:45:26 -0300

    first commit

---
+++ todo-app/TaskCell.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCell.swift
+//  todo-app
+//
+//  Created by Dulval on 02/11/18.
+//  Copyright © 2018 Dulval. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    @IBOutlet weak var checkBoxOutlet: UIButton!
+    @IBOutlet weak var taskNameLabel: UILabel!
+    
+    @IBAction func checkBoxAction(_ sender: Any) {
+    }
+}
